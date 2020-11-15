eae3a55c50802c595f85763a9b1763322aa2fce8
Prasad <Prasad@192.168.1.5>
2020-11-15 16:16:44 +0530

    My commit 8

---
+++ MyMavenProject8/src/main/java/Group8/MyMavenProject8/App.java
@@ -0,0 +1,13 @@
+package Group8.MyMavenProject8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
