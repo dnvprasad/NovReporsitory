0f333986414c5b3796f1577b309dc225dd6fcd52
Prasad <Prasad@192.168.1.5>
2020-11-15 13:37:05 +0530

    My first commit

---
+++ MyMavenProject6/src/main/java/MyGroup6/MyMavenProject6/App.java
@@ -0,0 +1,13 @@
+package MyGroup6.MyMavenProject6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
